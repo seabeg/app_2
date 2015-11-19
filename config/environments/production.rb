e1af68f70cf82ab13ffdb178918a2d2a308eb2f4
seabag <x72xakwmxoxo@gmail.com>
2015-11-19 06:53:52 +0000

    Add a StaticPages controller

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Rails.application.configure do
+App2.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
